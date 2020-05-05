cd43b38c1ba6e6c36c83926c1798bc78fa7e7ea3
Praani <praneshk92@gmail.com>
2020-05-06 02:13:03 +0530

    My New commit

--- src/main/java/Pages/Basepage.java
+++ src/main/java/Pages/Basepage.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Basepage {
@@ -27,7 +28,10 @@ public class Basepage {
 		if (browserName.equalsIgnoreCase("chrome")) {
 			//String path = System.getProperty("user.dir");
 			System.setProperty("webdriver.chrome.driver", "/home/pranesh/chromedriver");
-			driver = new ChromeDriver();
+			ChromeOptions ChromeOptions = new ChromeOptions();
+			ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+			driver = new ChromeDriver(ChromeOptions);
+			//driver = new ChromeDriver();
 		}
 		else if (browserName.equalsIgnoreCase("firefox")) {
 			String path = System.getProperty("user.dir");
